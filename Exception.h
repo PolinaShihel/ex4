7fb9cd936d1792a482a58ddca68bd5c5f95a7790
PolinaShihel <polinashihel@campus.technion.ac.il>
2022-06-17 20:08:05 +0300

    changed exception to noexcept

--- Exception.h
+++ Exception.h
@@ -6,7 +6,7 @@
 class DeckFileNotFound : public std::exception
 {
 public:
-    const char* what() const override
+    virtual const char* what() const noexcept override
     {
         return "Deck File Error: File not found";
     }
@@ -20,7 +20,7 @@ public:
     m_error("Deck File Error: File format error in line " + std::to_string(m_line))
     {}
 
-    virtual const char* what() const override
+    virtual const char* what() const noexcept override
     {
         return m_error.c_str();
     }
@@ -33,7 +33,7 @@ private:
 class DeckFileInvalidSize : public std::exception
 {
 public:
-    const char* what() const override
+    virtual const char* what() const noexcept override
     {
         return "Deck File Error: Deck size is invalid";
     }
@@ -42,7 +42,7 @@ public:
 
 class InvalidCardName : public std::exception {
 public:
-    const char* what() const override
+    virtual const char* what() const noexcept override
     {
         return "An invalid name has been entered for the current card";
     }
