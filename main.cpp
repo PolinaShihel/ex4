a799e96e8e2e230eadbff499ba8a055e09675f67
oresh <oresh@ORESH>
2022-06-19 00:33:04 +0300

    Fixed a compilation error in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "Mtmchkin.h"
-static const int MAX_NUMBER_OF_ROUNDS = 100
-int main(){
+static const int MAX_NUMBER_OF_ROUNDS = 100;
+
+int main()
+{
     Mtmchkin game("deck.txt");
     while(!game.isGameOver() && game.getNumberOfRounds() < MAX_NUMBER_OF_ROUNDS){
         game.playRound();
